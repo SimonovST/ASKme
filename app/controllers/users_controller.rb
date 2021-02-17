2eada259d9848b0cabfe4a7fd893d2a481015664
stepan <ya.simonov-stepan@yandex.ru>
2021-02-18 02:54:07 +0500

    Intermediate result. Returns the error: NoMethodError in Users#showЭ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,5 +14,9 @@ class UsersController < ApplicationController
       username: 'simens',
       avatar_url: 'https://sun9-68.userapi.com/qMj-e0AdKRJ9P6Insj9PFuSzjgvzxiHWsMZX6A/NC3RXg3Crec.jpg'
     )
+
+    @questions = [
+    Question.new(text: 'Как дела?', created_at: Date.parse('28.03.2021'))
+    ]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+# Методвозвращает ссылку на аватарку пользователя или стандартную аватарку
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
 end
