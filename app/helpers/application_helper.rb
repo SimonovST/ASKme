dc6a821fb0b5ee54e03f859e70522598b799aec8
stepan <ya.simonov-stepan@yandex.ru>
2021-02-19 02:44:11 +0500

    I'm thinking about a helper for declination of the question and probably the answer at once

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,16 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def self.sklonenie(number, begemot, begemota, begemotov, with_number = false)
+    ostatok = number % 10
+    ostatok100 = number % 100
+    prefix = ""
+    prefix = "#{number} " if with_number
+
+    return "#{prefix}#{begemotov}" if [11, 12, 13, 14].include?(ostatok100)
+    return "#{prefix}#{begemot}" if ostatok == 1
+    return "#{prefix}#{begemota}" if [2, 3, 4].include?(ostatok)
+    "#{prefix}#{begemotov}"
+  end
 end
