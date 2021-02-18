588aa32214ede9826658d265fed8dabe4df3acc1
stepan <ya.simonov-stepan@yandex.ru>
2021-02-19 00:14:37 +0500

    Edit the ask me app according to Lesson 49. Added styles, refactored views, expanded Gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Stepan',
+        username: 'SimonovST',
+        avatar_url: 'https://sun9-68.userapi.com/qMj-e0AdKRJ9P6Insj9PFuSzjgvzxiHWsMZX6A/NC3RXg3Crec.jpg'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun'
+      )
+    ]
   end
 
   def new
@@ -11,12 +24,15 @@ class UsersController < ApplicationController
   def show
     @user = User.new(
       name: 'Stepan',
-      username: 'simens',
+      username: 'SimonovST',
       avatar_url: 'https://sun9-68.userapi.com/qMj-e0AdKRJ9P6Insj9PFuSzjgvzxiHWsMZX6A/NC3RXg3Crec.jpg'
     )
 
     @questions = [
-    Question.new(text: 'Как дела?', created_at: Date.parse('28.03.2021'))
+    Question.new(text: 'Как дела?', created_at: Date.parse('28.03.2021')),
+    Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('28.03.2021'))
     ]
+
+    @new_question = Question.new
   end
 end
