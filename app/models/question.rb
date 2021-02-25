faf56c545dd7c2debed8e20cc17b4004bd115f7b
stepan <ya.simonov-stepan@yandex.ru>
2021-02-26 01:53:28 +0500

    Displays the name of the user who asked the question

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,7 +6,7 @@ class Question < ApplicationRecord
   # поймут это как просьбу найти в базе объект класса User со значением id,
   # который равен question.user_id.
   belongs_to :user
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', optional: true
   #Валидция по проверке максимальной длины текста вопроса (максимум 255 символов)
   validates :text, presence: true, length: { maximum: 255 }
 end
